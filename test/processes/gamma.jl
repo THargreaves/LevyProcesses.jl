b46d5cda044f542ca05399141d2b89ce46985970
Tim Hargreaves <tim.hargreaves@icloud.com>
2025-10-14 11:39:14 +0100

    Remove unit tests for broken Gamma process inverse Lévy measure

--- test/processes/gamma.jl
+++ test/processes/gamma.jl
@@ -9,18 +9,8 @@ using LevyProcesses
     test_process = GammaProcess(0.9, 1.1)
     test_x = 1.2
 
-    @test levy_tail_mass(test_process, test_x) ≈ quadgk(Base.Fix1(levy_density, test_process), test_x, Inf)[1]
-end
-
-@testitem "Gamma: Inverse Lévy tail mass" begin
-    using LevyProcesses
-    using Test
-
-    test_process = GammaProcess(0.9, 1.1)
-    test_x = 1.2
-
-    Γ = levy_tail_mass(test_process, test_x)
-    @test inverse_levy_tail_mass(test_process, Γ) ≈ test_x
+    @test levy_tail_mass(test_process, test_x) ≈
+        quadgk(Base.Fix1(levy_density, test_process), test_x, Inf)[1]
 end
 
 @testitem "Gamma: Dominating process" begin
@@ -31,8 +21,11 @@ end
     test_x = 1.2
 
     dominating_process = LevyProcesses.GammaDominatingProcess(0.9, 1.1)
-    @test levy_tail_mass(dominating_process, test_x) ≈ quadgk(Base.Fix1(levy_density, dominating_process), test_x, Inf)[1]
-    @test inverse_levy_tail_mass(dominating_process, levy_tail_mass(dominating_process, test_x)) ≈ test_x
+    @test levy_tail_mass(dominating_process, test_x) ≈
+        quadgk(Base.Fix1(levy_density, dominating_process), test_x, Inf)[1]
+    @test inverse_levy_tail_mass(
+        dominating_process, levy_tail_mass(dominating_process, test_x)
+    ) ≈ test_x
 end
 
 @testitem "Gamma: Rejection sampling" begin
@@ -55,31 +48,6 @@ end
         sample_jumps = sample(rng, p, test_t)
         marginal_samples[i] = sum(sample_jumps.jump_sizes)
     end
-
-    # Compare with ground truth
-    test = ExactOneSampleKSTest(marginal_samples, marginal(p.process, test_t))
-    @test pvalue(test) > 0.1
-end
-
-@testitem "Gamma: Inverse Lévy measure sampling" begin
-    using LevyProcesses
-    using HypothesisTests
-    using Random
-    using Test
-
-    test_process = GammaProcess(0.9, 1.1)
-    test_t = 0.8
-    test_ϵ = 1e-10
-
-    p = TruncatedLevyProcess(test_process; l=test_ϵ)
-    REPS = 1000
-    rng = MersenneTwister(1234)
-
-    # Generate samples
-    marginal_samples = [
-        sum(sample(rng, p, test_t, Inversion).jump_sizes)
-        for _ in 1:REPS
-    ]
 
     # Compare with ground truth
     test = ExactOneSampleKSTest(marginal_samples, marginal(p.process, test_t))
